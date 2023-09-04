dca6f626db212ed8ae3e7245378a5b787704b188
vit21513 <vit21513@yandex.ru>
2023-09-04 21:22:03 +0300

    add 5 homework

--- home_project/home_project/settings.py
+++ home_project/home_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +19,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--bu@w8apz%!ke%rjhz_&&^l=8#yqm8@rnj6n%=bd*(05um(yky'
+# SECRET_KEY = 'django-insecure--bu@w8apz%!ke%rjhz_&&^l=8#yqm8@rnj6n%=bd*(05um(yky'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+ALLOWED_HOSTS = [
+'127.0.0.1',
+'djangoMyproject.pythonanywhere.com',
+]
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
+
 
 # Application definition
 
@@ -71,10 +83,18 @@ WSGI_APPLICATION = 'home_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoMyproject$default',
+        'USER': 'djangoMyproject',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'djangoMyproject.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
